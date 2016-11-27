8933a8b5a9143a8ac81b4060d2728b204d21e9a8
Miky91 <migueldah91@gmail.com>
2016-11-27 22:49:21 +0100

    id3 acabado

--- untitled/ID3/id3.py
+++ untitled/ID3/id3.py
@@ -1,20 +1,21 @@
 # Insertar aqui la cabecera
 
-import Arbol
-import csv
 
+import csv
+import math
+
+class Nodo(object):
+    def __init__(self,attribute):
+
+        self.attr = attribute
+        self.list = list()
+        self.isHoja = False
 
 class Tupla(object):
-    def __init__(self, clase, rep):
-
-        self.rep = rep
-        self.clase = clase
-
-class Entropia(object):
-    def __init__(self, att, clase, prob):
+    def __init__(self, att, clase, rep):
         self.att = att
         self.clase = clase
-        self.entropia = prob
+        self.rep = rep
 
 
 class Instancia(object):
@@ -33,42 +34,169 @@ class ID3(object):
             self.attList = list()
             #lista de instancias
             self.instancias = list()
-            #valores de clase
+            #valores de clase y numero de veces repetidas
             self.classes = dict()
+            self.repeticionesValorAtr = dict()
             i = 0
             #lista de valores de tuplas
             nvaloresAtts = dict()
             self.valoresAtts = dict()
-            #tuplas repetidas
-            tuplasRepetidas = dict()
+            #tuplas repetidas clave nombre attr valor (att.value, class.value, n_reps)
+            self.tuplasRepetidas = dict()
+            self.n_instancias = 0
             for row in reader:
                 if i == 0:
                     self.attList = row
                     for elem in self.attList:
                         nvaloresAtts[elem] = 0
                         self.valoresAtts[elem] = list()
+                        self.repeticionesValorAtr[elem] = dict()
+
+                else:
+                    self.instancias.append(row)
                     if self.classes.has_key(row[len(self.attList)-1]):
                         self.classes[row[len(self.attList) - 1]]+=1
                     else:
-                        self.classes[row[len(self.attList) - 1]] =0
-                 else:
-                    self.instancias.append(row)
+                        self.classes[row[len(self.attList) - 1]] = 1
+                    k = 0
+                    while k < len(self.attList)-1:
+                        dic=self.repeticionesValorAtr[self.attList[k]]
+                        if dic.has_key(row[k]):
+                            dic[row[k]]+=1
+                        else:
+                            dic[row[k]] = 1
+                        t = Tupla(row[k],row[len(self.attList)-1],1)
+                        auxiliar = list(self.valoresAtts[self.attList[k]])
+                        encontrado = False
+                        for elem in auxiliar:
+                            if (elem.att == row[k]) & (elem.clase == row[len(self.attList)-1]):
+                                elem.rep+=1
+                                encontrado = True
+                        if encontrado is False:
+                            self.valoresAtts[self.attList[k]].append(t)
+                        k+=1
 
                 i+=1
+            self.n_instancias = i-1
+            self.arbol = self.id3(self.instancias,self.attList)
+
+
+
+    def selecattribute(self,conj, lista):
+
+        eInicial = 0
+        eTemporal = 0
+        atributo = ''
+        for key in self.classes:
+            aux = float(self.classes[key])/self.n_instancias
+            eInicial += math.fabs(aux)*math.log(aux)
+
+        auxiliar = float(0)
+        for key in self.valoresAtts:
+            if lista.count(key) != 0:
+                for value in self.valoresAtts[key]:
+                    aux = dict(self.repeticionesValorAtr[key])
+                    n_instancias_attr =aux[value.att]
+                    auxiliar += float(value.rep)/n_instancias_attr+math.fabs(math.log(float(value.rep)/n_instancias_attr))
+                    if (eTemporal > auxiliar) | (eTemporal == 0):
+                        eTemporal = aux
+                        atributo = key
+        return atributo
+
+
+
+
+    def particion(self,conjunto, atributo, valor):
+        lista = list()
+        for instancia in conjunto:
+            if instancia.count(valor) >0:
+                lista.append(instancia)
+        return lista
+
+
 
 
     def id3(self, conjunto, attList):
+        previous = 0
+        cp = ''
+        for elem in self.classes:
+            if previous < self.classes[elem]:
+                previous = self.classes[elem]
+                cp = elem
+        for instancia in self.instancias:
+            if (instancia[len(attList)-1] == cp) & (previous == self.n_instancias):
+                hoja = Nodo(cp)
+                hoja.isHoja = True
+                return cp
+            elif len(attList) == 0:
+                hoja = Nodo(cp)
+                hoja.isHoja = True
+                return cp
+            a = self.selecattribute(conjunto,attList)
+            nodo = Nodo(a)
+            valores = self.repeticionesValorAtr[a].keys()
+            for value in valores:
+                cj= self.particion(conjunto,a,value)
+                if not cj:
+                    n = Nodo(cp)
+                    n.isHoja = True
+                else:
+                    nuevosAtributos = list()
+                    for atributo in attList:
+                        if atributo != "class":
+                            if atributo != a:
+                                nuevosAtributos.append(atributo)
+                    n = self.id3(cj,nuevosAtributos)
+                nodo.list.append((n,value))
+            return nodo
 
-        pass
+    def buscarClase(self,arbol, value):
+        for elem in arbol.list:
+            if elem[1] == value:
+                return elem[0]
+
 
 
     def clasifica(self, instancia):
-        pass
+        if self.arbol.isHoja:
+            print "Clase predicha: "+self.arbol.attr
+        else:
+            #keys = instancia.keys()
+            auxArbol = self.arbol
+            #buscarClase(auxArbol, instancia)
+            k = 0
+            while k < len(instancia.keys()):
+                if auxArbol.isHoja:
+                    print "Clase predicha: "+auxArbol.attr
+                else:
+                    auxArbol = self.buscarClase(auxArbol, instancia[auxArbol.attr])
+                k+=1
+
+        print "Clase predicha: "+auxArbol
+
 
     def test(self, fichero):
-        pass
+        with open(fichero, 'rb') as csvfile:
+            dialect = csv.Sniffer().sniff(csvfile.read(), delimiters=",")
+            csvfile.seek(0)
+            reader = csv.reader(csvfile, dialect)
+            attList = list()
+            valoresAtts = dict()
+            i = 0
+            aciertos = 0
+            fallos = 0
+            for row in reader:
+                if i == 0:
+                    attList = row
+                    for elem in attList:
+                        valoresAtts[elem] = list()
+                    i = i + 1
+                else:
+                    instancia = row
+                    self.clasifica(instancia)
 
     def save_tree(self, fichero):
         pass
 
 id3 = ID3("train.data")
+id3.clasifica({'season' : 'winter','wind' : 'high', 'day' : 'weekday', 'rain' : 'heavy'})
